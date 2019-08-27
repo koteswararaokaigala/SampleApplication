47481f4bc070e19a1bcf144224d7ffac399d725e
kaigalakoteswararao <koteswararaokaigala@gmail.com>
2019-08-27 20:57:25 +0530

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
 	def index
 		@books = Book.all.order("created_at DESC")
+	end
+	def show 
 	end
         def new
 		@book = Book.new
@@ -13,10 +16,28 @@ class BooksController < ApplicationController
 			render 'new'
 		end
 	end
+	def edit
+	end
+	def update 
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+
+	end
+	def destroy
+		@book.destroy
+		redirect_to root_path
+	end
+
 	 
 	private 
 	def book_params
 		params.require(:book).permit(:title, :description, :author)
+	end
+	def find_book
+		@book  = Book.find(params[:id])
 	end
 
 
